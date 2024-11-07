940e93e89900b5849c00230c9746db0a151e401b
er-zou <157382503+er-zou@users.noreply.github.com>
2024-11-07 15:44:17 -0500

    Create hw4.c

---
+++ hw4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#inlcude <sys/socket.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+
+
+  return 0;
+}
